c7f542b9fddba0e7c17c2c3bd4030a6e6536f4f6
rauf <33981808+rafucuk@users.noreply.github.com>
2024-02-02 18:38:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
-    name='ruhsub',
-    version='0.1.0',
-    packages=find_packages(),
+    name='autosub',
+    version='0.4.0',
+    description='Auto-generates subtitles && edites video audio for any video or audio file',
+    author='Rauf ŞEn',
+    author_email='raufsen11@gmail.com',
+    url='https://github.com/rafucuk/ruhsub',
+    packages=['ruhsub'],
+    entry_points={
+        'console_scripts': [
+            'ruhsub = ruhsub:main',
+        ],
+    },
     install_requires=[
         'torch==1.10.0+rocm',
         'googletrans==4.0.0-rc1',
@@ -13,4 +28,5 @@ setup(
         'TTS==0.22.0',
         'pysrt==1.1.2',
     ],
+    license=open("LICENSE").read()
 )
