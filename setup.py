12e934e2831b2787cb01ce35395ee38d7ad9bf8a
rauf <33981808+rafucuk@users.noreply.github.com>
2024-02-02 18:30:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
+        'torch==1.10.0+rocm',
         'googletrans==4.0.0-rc1',
-        'deepl==2.1.0',
+        'deepl==1.16.1',
         'moviepy==1.0.3',
         'whisper_timestamped==0.1.0',
-        'TTS==0.0.1',
-        'pysrt==1.1.1',
+        'TTS==0.22.0',
+        'pysrt==1.1.2',
     ],
 )
